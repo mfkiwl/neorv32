301fdfa463c2e74c9d49f660c3d19057479c3550
GHA <push@gha>
2025-01-11 08:06:35 +0000

    update 9b68c61e4c0b32527242ce65a9f5a6d4fb50f801

---
+++ sw/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_5f_5fneorv32_5frte_5fprint_5fhex_0',['__neorv32_rte_print_hex',['../neorv32__rte_8c.html#af779316b2f04b01ca413fe9fdffcf6c8',1,'neorv32_rte.c']]],
+  ['_5f_5fneorv32_5fxirq_5fcore_1',['__neorv32_xirq_core',['../neorv32__xirq_8c.html#a14661d628dd31e6bfce89376c6614627',1,'neorv32_xirq.c']]],
+  ['_5f_5fneorv32_5fxirq_5fdummy_5fhandler_2',['__neorv32_xirq_dummy_handler',['../neorv32__xirq_8c.html#ae4d9362b23a1341c595a4cd239a87091',1,'neorv32_xirq.c']]]
+];
---
+++ sw/search/variables_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['eie_0',['EIE',['../structneorv32__xirq__t.html#aef3c2b491207abd68bf6392f97096a74',1,'neorv32_xirq_t']]],
+  ['error_5fmessage_1',['error_message',['../bootloader_8c.html#a9b511a5d68314efde0050f19848e1aac',1,'bootloader.c']]],
+  ['esc_2',['ESC',['../structneorv32__xirq__t.html#ae4f7f3f41210ff329b6f82d5dfada88a',1,'neorv32_xirq_t']]],
+  ['exe_5favailable_3',['exe_available',['../bootloader_8c.html#a0f9fcfdf758260b9b79b6eda063dd87a',1,'bootloader.c']]]
+];
